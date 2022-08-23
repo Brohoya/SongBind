dbead16d82d2b2d71d10bc83ac57779c6d290ee4
Brohoya <b.elgart@net.estia.fr>
2022-08-23 12:40:24 +0200

    vercel deploy fix

---
+++ src/pages/api/spotify/get/songs.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+var Cookies = require('cookies');
+
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+
+
+
+
+    res.status(200);
+}
--- src/pages/api/spotify/get/topTracks.ts
+++ src/pages/api/spotify/get/topTracks.ts
@@ -1,16 +1,11 @@
-import { getSpotifyTopTracks } from '../../../../lib/Spotify';
+import { NextApiRequest, NextApiResponse } from "next";
+var Cookies = require('cookies');
 
-export default async (req, res) => {
-  const response = await getSpotifyTopTracks();
-  const { items } = await response.json();
 
-  console.log(items);
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
 
-  // const tracks = items.slice(0, 10).map((track) => ({
-  //   artist: track.artists.map((_artist) => _artist.name).join(', '),
-  //   songUrl: track.external_urls.spotify,
-  //   title: track.name
-  // }));
 
-  // return res.status(200).json({ tracks });
-};
+
+
+    res.status(200);
+}
